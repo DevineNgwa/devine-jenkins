e3bea19c09102a58368f792a23b6f10284377658
devine <ambedevine26@gmail.com>
2025-06-09 03:03:16 -0400

    change made

--- terraform.tf
+++ terraform.tf
@@ -46,5 +46,8 @@ resource "aws_instance" "web" {
   key_name                    = var.key_name
   associate_public_ip_address = true
 
-  
+  tags = {
+    Name = "example-instance"
+    Env  = "dev"
+  }
 }
